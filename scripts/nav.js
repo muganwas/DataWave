082fdb43d0fbd89a9f633c62362d4a836d8a79b5
Muganwa <stevenmuganwa@live.com>
2023-10-06 23:13:31 +0300

    added fonts

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,12 +1,22 @@
 (function (doc) {
     const links = doc.getElementsByClassName('link');
+    const linksContainer = doc.getElementById('links');
+    const hamburger = doc.getElementById('hamburger');
     for (let i = 0; i < links.length; i++) {
         const link = links[i];
         link.addEventListener('click', (e) => {
             Array.from(links).forEach(lnk => lnk.classList.remove('active'));
             const target = e.target;
             target.classList.add('active');
+            linksContainer.style.display = null;
         })
     }
-
+    hamburger.addEventListener('click', function (e) {
+        e.preventDefault();
+        const v = linksContainer.style.display;
+        if (!v) {
+            linksContainer.style.display = 'flex';
+        } else
+            linksContainer.style.display = null;
+    });
 })(document);
